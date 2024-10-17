7f3a43db8e842f1a2e65c769c7710a3200fca256
zhyswan <www.nova@gmail.com>
2024-10-17 10:51:59 +0800

    element

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
+  <div> 11
+    <el-button type="primary">按钮</el-button>
+    {{ count }}
+    <!-- <a-button>
+    button
+  </a-button> -->
     <NuxtWelcome />
+
   </div>
 </template>
+<script setup lang="ts">
+const count = ref(0);
+console.log(count.value++);
+
+// import.meta.client && alert(count.value++);
+
+
+// const config = useRuntimeConfig();
+// console.log(config);
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: ["@/assets/styles/scrollbar.css", "@/assets/styles/base.css"],
+
+  // 运行时全局变量，在server都可以访问
+  // 在client端只能看到public
+  runtimeConfig: {
+    count: 0,
+
+    // 在client可以看到的变量
+    public: {
+      apiUrl: process.env.API_URL,
+    },
+  },
+
+  modules: ["@element-plus/nuxt"]
+});
